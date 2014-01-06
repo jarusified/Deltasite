0f4e77db2a5dc7d3cfc1ccf9670b18bcbc3071ee
suraj <jarus3001@gmail.com>
2014-01-07 04:19:11 +0530

    page3 changed

--- js/page3/main.js
+++ js/page3/main.js
@@ -1,29 +1,40 @@
-
+(function($){
 var Renderer = function(canvas){
 	var canvas = $(canvas).get(0);
 	var ctx    = canvas.getContext("2d");
-	var ps;
+	var ps;				
+	var nearest=null;	
+
 
 	canvas.width = window.innerWidth;
 	canvas.height= window.innerHeight;
 
 	var gfx = arbor.Graphics(canvas);
 
-	return {
+	var that={
 		init:function(system){
 			ps = system;
 			ps.screen({size:{width : canvas.width, height:canvas.height}, padding:[36,80,30,26]});
 			ps.screenPadding(80);
+			that.resize();						
+			that.initMouseHandling();	
 
-			this.initMouseHandling();	
 		},
+		 resize:function(){
+        canvas.width = $(window).width()
+        canvas.height = .75* $(window).height()
+        ps.screen({size:{width:canvas.width, height:canvas.height}})
+        that.redraw()
+      },
 
 		redraw:function(){
+			//console.log('rfv');
 			gfx.clear();
 			var color = "white";
 			ps.eachEdge(function(line, sp, tp){
 				if(line.source.data.alpha * line.target.data.alpha == 0) return
 				gfx.line(sp, tp, {stroke:"#b2b19d", width:2 });
+				//console.log(line);
 			});
 
 			ps.eachNode(function(node, pt){
@@ -31,78 +42,83 @@ var Renderer = function(canvas){
 				if(node.data.alpha == 0) return;
 
 				if(node.data.shape == "dot"){	
-					gfx.oval(pt.x-w/2, pt.y-w/2, w, w, {fill:node.data.color});	
+					gfx.oval(pt.x-w/2, pt.y-w/2, w, w, {fill:node.data.color, alpha:node.data.alpha});	
 					gfx.text(node.name, pt.x, pt.y+7, {color:color, align:"center", font:"Arial", size:22});
 				}else{
-					gfx.rect(pt.x - w/2, pt.y - w/8, w, 20, 4,{fill:node.data.color});
+					gfx.rect(pt.x - w/2, pt.y - w/8, w, 20, 4,{fill:node.data.color, alpha:node.data.alpha});
 					gfx.text(node.name, pt.x, pt.y+7, {color:color, align:"center", font:"Arial", size:22});
 				}
 			});
 
 		},
 
+		switchsection:function(newsec){
+			var parent = ps.getEdgesFrom(nearest.node.name)[0].source;
+			var children = $.map(ps.getEdgesFrom(nearest.node.name), function(edge){
+			return edge.target;
+				});
+			
+			ps.eachNode(function(node){
+				if(node.data.shape=='dot') return 
+				var nowVisible=($.inArray(node, children)>=0);
+				var newAlpha= (nowVisible) ? 1 :0;
+				var dt=(nowVisible) ? 0.5 :0.5;
+				ps.tweenNode(node,dt,{alpha:newAlpha});
+
+				if(newAlpha==1){
+					node.p.x= parent.p.x +.05*Math.random() -0.025;
+					node.p.y= parent.p.y +0.5*Math.random() -0.025;
+					node.tempMass=.001;
+ 				}
+			});
+
+		},	
+
 		initMouseHandling:function(){
 			var dragged = null;
-
+			    nearest = null;
+			    selected=null;
 			var handler = {
 				moved:function(e){
+					 section = null;
 					var pos = $(canvas).offset();
 					_mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
-
-					var nearest = ps.nearest(_mouseP).distance > 50 ? ps.nearest(_mouseP) : null;
+					nearest	 = ps.nearest(_mouseP);
 					if(!nearest.node) return ;
-						//console.log(nearest.node.name);
-						
-						var parent = ps.getEdgesFrom(nearest.node.name)[0].source;
-						var children = $.map(ps.getEdgesFrom(nearest.node.name), function(edge){
-						return edge.target;
-
-					});
-
-					ps.eachNode(function(node){
-						if(node.data.shape == "dot") return;
-
-						var nowvis = ($.inArray(node, children)>=0);
-						var newAlpha = nowvis ? 1 : 0;
-						var dt = nowvis ? .5: .5;
-
-						ps.tweenNo3de(node, dt, {alpha:newAlpha});
-
-
-						if (newAlpha==1){
-	            			node.p.x = parent.p.x + .05*Math.random() - .025;
-	            			node.p.y = parent.p.y + .05*Math.random() - .025;
-	          				node.tempMass = .001;
-	          			}					
-					});
-
-
+					
+					if(nearest.node.name!='hoverme'){
+						console.log(nearest);
+						selected=(nearest.distance<10)? nearest :null;		
+						}	
+					else if(nearest.node.name=='hoverme'){
+						if(nearest.node.name!=section){
+								section=nearest.node.name;
+								that.switchsection(section);
+						}	
+					}
 				},
 
 				clicked:function(e){
 					var pos = $(canvas).offset();
 					_mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
+					nearest=dragged=ps.nearest(_mouseP);
 
-					var node = ps.nearest(_mouseP).distance < 50 ? ps.nearest(_mouseP) : null;
-
-					dragged = ps.nearest(_mouseP);
-
-					if(dragged && dragged.node !== null){
-						dragged.node.fixed = true;
+					if(nearest && selected && nearest.node==selected.node){
+						var link=selected.node.data.link;
+						return false;
 					}
 
-					
-					if(node.node && node.node.data.link != undefined){
-						window.location = node.node.data.link;
+					if(dragged && dragged.node !== null){
+						dragged.node.fixed=true;
 					}
 
 					$(canvas).unbind('mousemove', handler.moved);
-
 					$(canvas).bind('mousemove', handler.dragged);
 					$(canvas).bind('mouseup', handler.dropped);
 				},
 
 				dragged:function(e){
+					var old_nearest=nearest&& nearest.node._id;
 					var pos = $(canvas).offset();
 					var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);				
 					
@@ -133,91 +149,105 @@ var Renderer = function(canvas){
 		
 		}
 	};
-};
+	return that;
+}
 
 
 
 
 $(document).ready(function(){
-	var sys = arbor.ParticleSystem({stiffness:900, repulsion:2000, dt: 0.015});
-		sys.parameters({gravity:true});
-		sys.renderer = Renderer('#canvas');
-
 	var ui = {
 		nodes:{
-			"delta":{
+			hoverme:{
 				color:"red", shape:"dot", alpha:1
 			},
 
-			about:{
-				color:"green",
-				shape:"dot",
-				alpha:1
+			'1':{
+				color:"brown",
+				
+				alpha:0,
+				
 			},
 
-			blog:{
-				color:"cyan",
-				shape:"dot",
-				alpha:1
-
-			},
-
-			projects:{
-				color: "brown",
-				shape:"dot",
-				alpha:1
-			},
-
-			contact:{
-				color:"gold",
-				shape:"dot",
-				alpha:1
-			},
-
+			'2':{
+				color:"brown",
 			
-			alink:{color:"black", shape:"rect", alpha:1, link:"http://google.com/"},
-			blink:{color:"black", shape:"rect", alpha:1},
-			plink:{color:"black", shape:"rect", alpha:1},
-			clink:{color:"black", shape:"rect", alpha:1}
+				alpha:0,
+				
+			},
+
+			'3':{
+				color:"brown",
+		
+				alpha:0,
+				
+			},
+
+			'4':{
+				color:"brown",
+
+				alpha:0,
+				
+			},
+
+			'5':{
+				color:"brown",
+				
+				alpha:0,
+				
+			},
+
+			'6':{
+				color:"brown",
+				
+				alpha:0,
+				
+			},
+
+			'7':{
+				color:"brown",
+				
+				alpha:0,
+				
+			},
+
+			'8':{
+				color:"brown",
+				
+				alpha:0,
+				
+			},
+
+			'9':{
+				color:"brown",
+				alpha:0,
+				
+			},
+
+			'10':{
+				color:"brown",
+				alpha:0,
+			}
 		},
 
 		edges:{
-			"delta":{
-				about:{},
-				blog:{},
-				projects:{},
-				contact:{},
-			},
-			about:{
-				about:{},
-				blog:{},
-				projects:{},
-				contact:{},
-			    alink:{}
-			},
-			blog:{
-				about:{},
-				blog:{},
-				projects:{},
-				contact:{},
-				blink:{}
-			},
-			projects:{
-				about:{},
-				blog:{},
-				projects:{},
-				contact:{},
-				plink:{}
-			},
-			contact:{
-				about:{},
-				blog:{},
-				projects:{},
-				contact:{},
-				"clink":{}
+			hoverme:{
+				'1':{},
+				'2':{},
+				'3':{},
+				'4':{},
+				'5':{},
+				'6':{},
+				'7':{},
+				'8':{},
+				'9':{},
+				'10':{}
 			}
 		}
 	};
-
-	sys.graft(ui);
-}); 
+	var sys = arbor.ParticleSystem({stiffness:900, repulsion:2000, dt: 0.015});
+		sys.parameters({gravity:true});
+		sys.renderer = Renderer('#canvas');
+		sys.graft(ui);
+});
+})(this.jQuery);
